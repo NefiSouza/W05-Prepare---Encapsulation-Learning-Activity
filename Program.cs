d03980a0e2386c151c98f10171dc7f00cf192e2d
NefiSouza <122121591+NefiSouza@users.noreply.github.com>
2023-03-25 21:27:00 -0600

    yo1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathAssignment assignment1 = new MathAssignment();
+        assignment1.SetTopic("Math");
+        assignment1.SetStudentName("Jennifer Laurence");
+        assignment1.SetTextBookSection("7.8");
+        assignment1.SetProblems("3-5");
+        string homework = assignment1.GetHomeworkList();
+        string summary = assignment1.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine(homework);
+
+        Console.WriteLine(" ");
+
+        WritingAssignment assignment2 = new WritingAssignment();
+        assignment2.SetTopic("European History");
+        assignment2.SetStudentName("Mary Waters");
+        assignment2.SetTitle("The Causes of World War II");
+        summary = assignment2.GetSummary();
+        string topicHead = assignment2.GetWritingInformation();
+        Console.WriteLine(summary);
+        Console.WriteLine(topicHead);
+
+    }
+}
